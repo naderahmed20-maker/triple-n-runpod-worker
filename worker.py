b98270a4b53aff810570fd2abd519a79d69f88cd
naderahmed20-maker <39351nader.ahmed20@icloud.com>
2026-07-08 20:00:12 +0200

    Switch worker to BiRefNet

--- worker.py
+++ worker.py
@@ -1,6 +1,16 @@
-import runpod
-from handler import handler
+import traceback
 
-runpod.serverless.start({
-    "handler": handler
-})
+try:
+    import runpod
+    from handler import handler
+
+    print("Worker starting...")
+    runpod.serverless.start({
+        "handler": handler
+    })
+
+except Exception as e:
+    print("WORKER STARTUP ERROR:")
+    print(str(e))
+    traceback.print_exc()
+    raise
